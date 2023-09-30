1c46d4cf1ad0865d3beac394c053be3b197e385d
Timo Eberl <timo@timoeberl.de>
2023-09-30 16:29:39 +0200

    Ocean v1

---
+++ Assets/Shaders/ocean.hlsl
@@ -0,0 +1,142 @@
+// implementation of MurmurHash (https://sites.google.com/site/murmurhash/) for a 
+// single unsigned integer.
+
+uint hash(uint x, uint seed) {
+    const uint m = 0x5bd1e995U;
+    uint hash = seed;
+    // process input
+    uint k = x;
+    k *= m;
+    k ^= k >> 24;
+    k *= m;
+    hash *= m;
+    hash ^= k;
+    // some final mixing
+    hash ^= hash >> 13;
+    hash *= m;
+    hash ^= hash >> 15;
+    return hash;
+}
+
+// implementation of MurmurHash (https://sites.google.com/site/murmurhash/) for a  
+// 2-dimensional unsigned integer input vector.
+
+uint hash(float2 x, uint seed){
+    const uint m = 0x5bd1e995U;
+    uint hash = seed;
+    // process first vector element
+    uint k = x.x; 
+    k *= m;
+    k ^= k >> 24;
+    k *= m;
+    hash *= m;
+    hash ^= k;
+    // process second vector element
+    k = x.y; 
+    k *= m;
+    k ^= k >> 24;
+    k *= m;
+    hash *= m;
+    hash ^= k;
+	// some final mixing
+    hash ^= hash >> 13;
+    hash *= m;
+    hash ^= hash >> 15;
+    return hash;
+}
+
+
+float2 gradientDirection(uint hash) {
+    switch (int(hash) & 3) { // look at the last two bits to pick a gradient direction
+    case 0:
+        return float2(1.0, 1.0);
+    case 1:
+        return float2(-1.0, 1.0);
+    case 2:
+        return float2(1.0, -1.0);
+    case 3:
+        return float2(-1.0, -1.0);
+    }
+}
+
+float interpolate(float value1, float value2, float value3, float value4, float2 t) {
+    return lerp(lerp(value1, value2, t.x), lerp(value3, value4, t.x), t.y);
+}
+
+float2 fade(float2 t) {
+    // 6t^5 - 15t^4 + 10t^3
+	return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
+}
+
+float perlinNoise(float2 position, uint seed) {
+    float2 floorPosition = floor(position);
+    float2 fractPosition = position - floorPosition;
+    float2 cellCoordinates = float2(floorPosition);
+    float value1 = dot(gradientDirection(hash(cellCoordinates, seed)), fractPosition);
+    float value2 = dot(gradientDirection(hash((cellCoordinates + float2(1, 0)), seed)), fractPosition - float2(1.0, 0.0));
+    float value3 = dot(gradientDirection(hash((cellCoordinates + float2(0, 1)), seed)), fractPosition - float2(0.0, 1.0));
+    float value4 = dot(gradientDirection(hash((cellCoordinates + float2(1, 1)), seed)), fractPosition - float2(1.0, 1.0));
+    return interpolate(value1, value2, value3, value4, fade(fractPosition));
+}
+
+float perlinNoise(float2 position, int frequency, int octaveCount, float persistence, float lacunarity, uint seed) {
+    float value = 0.0;
+    float amplitude = 1.0;
+    float currentFrequency = float(frequency);
+    uint currentSeed = seed;
+    for (int i = 0; i < octaveCount; i++) {
+        currentSeed = hash(currentSeed, 0x0U); // create a new seed for each octave
+        value += perlinNoise(position * currentFrequency, currentSeed) * amplitude;
+        amplitude *= persistence;
+        currentFrequency *= lacunarity;
+    }
+    return value;
+}
+
+void ocean_float(
+	float2 screen_position, float time, float number_of_waves,
+	float wave_length, float wave_width, float wave_chaoticness,
+	float scroll_speed, float3 background_color, float3 wave_color,
+	out float3 out_color)
+{
+	float ratio = 16.0/9.0;
+	screen_position.x = 1 - screen_position.x;
+	screen_position.x *= ratio;
+	screen_position.x += + scroll_speed/number_of_waves * time;
+	screen_position *= 100;
+	screen_position = floor(screen_position);
+	screen_position /= 100;
+	float x = screen_position.x * number_of_waves;
+
+	uint seed = 0x578437adU;
+	float noise_frequency = 1;
+	float2 noise_coord = float2(x, screen_position.y * number_of_waves);
+	noise_coord.x += time * 0.1;
+	float perlin_noise = perlinNoise(noise_coord, noise_frequency, 3, 0.5, 2.0, seed);
+	noise_coord.x -= time * 0.3;
+	float perlin_noise_3 = perlinNoise(noise_coord*0.6, seed);
+	noise_coord.x += time * 0.2;
+	float perlin_noise_2 = perlinNoise(noise_coord*0.4, seed);
+	
+	float wave_distance = 0.1;
+	float o = 1 - wave_distance;
+	float number_vertical_waves = 1/wave_width;
+	float y = screen_position.y;
+	float wave_mask = (sin((y+perlin_noise_3*0.07)*number_vertical_waves * 2*PI) + o) / (1+o);
+	wave_mask = max(0,wave_mask);
+	float wave_offset = wave_mask * 0.3;
+
+	float intensity = frac(x + wave_offset + perlin_noise_2*0.5 + perlin_noise*wave_chaoticness);
+	intensity = 1 - intensity;
+	float wave_id = ceil(x + wave_offset + perlin_noise*wave_chaoticness);
+
+	intensity = smoothstep(1-wave_length, 1, intensity);
+	intensity = lerp(0, intensity, smoothstep(0,0.5,wave_mask));
+	intensity = clamp(intensity, 0, 1);
+
+	out_color = intensity;
+	out_color.gb *= fmod(wave_id,2);
+
+	out_color = lerp(background_color, wave_color, intensity);
+	out_color = lerp(out_color, 1, smoothstep(0.9,1,intensity));
+}
