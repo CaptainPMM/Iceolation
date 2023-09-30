bb3beff9fc0d134b0e702f94600b11360a069022
Frederik Omlor <freddy-omlor@web.de>
2023-09-30 19:30:43 +0200

    Added item generation.

---
+++ Assets/Scripts/ItemSpawner/ItemSpawner.cs
@@ -0,0 +1,56 @@
+using LD54.Floatables.Floes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LD54.ItemGenerator {
+
+    public class ItemSpawner : MonoBehaviour {
+
+        [SerializeField]
+        private List<GameObject> itemPrefabs = new();
+
+        [SerializeField]
+        private Transform upperBound;
+
+        [SerializeField]
+        private Transform lowerBound;
+
+        [SerializeField]
+        private float minTimeBetweenSpawns = 4f;
+
+        [SerializeField]
+        private float maxTimeBetweenSpawns = 12f;
+
+        private bool isRunning = true;
+
+        void Start() {
+            StartCoroutine(SpawnObjects());
+        }
+
+        private IEnumerator SpawnObjects() {
+            while(isRunning) {
+                SpawnObject();
+                yield return WaitBetweenSpawn();
+            }
+        }
+
+        private IEnumerator WaitBetweenSpawn() {
+            yield return new WaitForSeconds(Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns));
+        }
+
+        private void SpawnObject() {
+            Vector3 spawnPosition = Vector3.Lerp(lowerBound.transform.position, upperBound.transform.position, Random.Range(0.0f, 1.0f));
+
+            GameObject item = Instantiate(itemPrefabs[Random.Range(0, Mathf.FloorToInt(itemPrefabs.Count))], spawnPosition, Quaternion.identity);
+
+
+            // Object specific initialization
+            if(item.TryGetComponent<FloeTile>(out FloeTile floeTile)) {
+                floeTile.IsFloating = true;
+            }
+
+
+        }
+    }
+}
