9e7e407100315f8d621bc2eb5be67b0889de51d7
Timo Eberl <timo@timoeberl.de>
2023-10-02 00:40:47 +0200

    Lower wave spawn rate to improve performance

--- Assets/Scripts/Floatables/Obstacles/Iceberg.cs
+++ Assets/Scripts/Floatables/Obstacles/Iceberg.cs
@@ -39,12 +39,12 @@ namespace LD54.Floatables.Obstacles
                 Destroy(gameObject);
             }
 
-            float waveProbability = (2.5f + Weight*2.0f) * MoveSpeed * Time.deltaTime;
+            float waveProbability = (1.5f + Weight*0.5f) * MoveSpeed * Time.deltaTime;
             if (Random.Range(0.0f, 1.0f) < waveProbability)
             {
                 Vector3 posOffset = new Vector3(Weight * 0.1f, -0.0f, 0.0f);
-                float duration = 1f / MoveSpeed + Weight*2.0f;
-                float size = 6.0f + Weight*4.0f;
+                float duration = 1f / MoveSpeed + Weight*1.0f;
+                float size = 6.0f + Weight*2.0f;
                 GameManager.Instance.Ocean.CreateWave(
                     this.transform.position + posOffset, 0.2f, size, duration, 1.0f, Ocean.Ocean.Shape.Trail
                 );
