cbc14f51e3e7ba57eaa697aac7dbf32276755344
Timo Eberl <timo@timoeberl.de>
2023-10-01 17:59:48 +0200

    Footstep left right offset

--- Assets/Scripts/Player/Steps.cs
+++ Assets/Scripts/Player/Steps.cs
@@ -15,8 +15,22 @@ public class Steps : MonoBehaviour
         if (direction == Vector2.zero) direction = Vector2.down;
 
         left = !left;
-        Debug.Log((left ? "Left" : "Right") + " step!, Direction: " + direction);
-        CreateStep(this.transform.position);
+
+        Vector2 footstepPosition = this.transform.position;
+        Vector2 directionRight = new (direction.y, -direction.x);
+
+        Vector2 footstepOffset = new(0.2f, 0.1f);
+
+        if (left)
+        {
+            footstepPosition -= directionRight * footstepOffset;
+        }
+        else
+        {
+            footstepPosition += directionRight * footstepOffset;
+        }
+
+        CreateStep(footstepPosition);
     }
 
     private PlayerController _playerController;
@@ -25,15 +39,17 @@ public class Steps : MonoBehaviour
 
     private void CreateStep(Vector3 wsPosition)
     {
-        ActiveStep step = new();
-        step.progress = 0.0f;
-        step.lifeTime = 1.0f;
-        step.instance = Instantiate(StepPrefab);
+        ActiveStep step = new()
+        {
+            progress = 0.0f,
+            lifeTime = 1.0f,
+            instance = Instantiate(StepPrefab)
+        };
 
         step.instance.transform.parent = StepContainer.transform;
         step.instance.transform.position = wsPosition;
-        // step.instance.GetComponent<SpriteRenderer>().material
-        //     .SetFloat("", x);
+        step.instance.GetComponent<SpriteRenderer>().material
+            .SetFloat("_Progress", step.progress);
 
         steps.Add(step);
     }
